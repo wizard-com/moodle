c1078bf4a65a7837a970c825ce6569dcf9b58609
18045945 <aungmoethet2000@gmail.com>
2020-09-24 18:30:04 +0800

    Displaying courses -> done Displaying course details -> In progress

--- local/message/course_details.php
+++ local/message/course_details.php
@@ -2,18 +2,20 @@
 
 require_once(__DIR__."/../../config.php");
 //
-$PAGE->set_url(new moodle_url("/local/message/courses.php"));
+$PAGE->set_url(new moodle_url("/local/message/course_details"));
 $PAGE->set_context(\context_system::instance());
-$PAGE->set_title("Courses");
+$PAGE->set_title("Course details");
 
 global $DB;
 //global $USER;
 //
 //$currentUserID = $USER->id;
 
-$courses = $DB->get_records_sql('SELECT id, fullname, enddate FROM {course} WHERE id != 1;');
+$courses = $DB->get_records_sql('SELECT fullname, enddate FROM {course} WHERE id != 1;');
 //$info = get_fast_modinfo($course);
 //print_object($info);
+
+
 foreach ($courses as $course){
     $course->enddate = substr(date('d-M-Y', $course->enddate), 0, 6);
 }
@@ -26,5 +28,5 @@ $templatecontext = (object)[
 ];
 
 echo $OUTPUT->header();
-echo $OUTPUT->render_from_template("local_message/courses", $templatecontext);
+echo $OUTPUT->render_from_template("local_message/course_details", $templatecontext);
 echo $OUTPUT->footer();
